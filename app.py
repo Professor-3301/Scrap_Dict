30eff2b8423ca0f6a93e760de2c927e1685fe869
Adwaith Anil <adwaith00adwaith@gmail.com>
2024-06-17 20:29:44 +0530

    More And More

--- app.py
+++ app.py
@@ -14,9 +14,6 @@ def index():
 def webscrap():
     return render_template('webscrap.html')
 
-# @app.route("/scrapetest")
-# def scrape():
-#     return render_template('scrape.html')
 
 @app.route("/contact")
 def contact():
@@ -32,37 +29,51 @@ def aboutus():
 
 @app.route('/directory' , methods=['GET','POST']) 
 def directory():
+    
     if request.method == 'POST':
         url = request.form['url']
-        directories_list = dirb_enum(url,200)
-        return render_template('result_dirb.html', data=directories_list)
-    elif request.method == 'GET':
-        return render_template('directory.html')
+        enumerate_directories_flag = 'enumerate' in request.form
 
+        if enumerate_directories_flag:
+            directories = dirb_enum(url)
+            all_scraped_data = []
 
+            for directory in directories:
 
-@app.route('/scrape', methods=['GET','POST'])
-def scrape():
-    if request.method == 'POST':
-        if request.method == 'POST':
-        # Get the form data
-            url = request.form['url']
-            scraping_option = request.form['scraping-option']
+                scraped_data = scrape_hyperlinks(directory)
+                all_scraped_data.append(directory)
 
-        # Perform scraping logic based on the selected option
+                all_scraped_data.append(scraped_data)
+                all_scraped_data.append('\n')
+            
+            
+            return render_template('result.html', data=all_scraped_data)
+        
+        else:
             if scraping_option == 'image-scraping':
                 scrape_images(url)
                 return send_file('./images.zip', as_attachment=True)
             elif scraping_option == 'link-scraping':
                 scraped_data = scrape_hyperlinks(url)
                 return render_template('result.html', data=scraped_data)
-        # Render the result template with scraped data
-            
-    elif request.method == 'GET':
-        return render_template('scrape.html')
     
+    return render_template('directory.html')
+
+
+@app.route('/scrape', methods=['GET', 'POST'])
+def scrape():
+    if request.method == 'POST':
+        url = request.form['url']
+        scraping_option = request.form['scraping-option']
+        if scraping_option == 'image-scraping':
+            scrape_images(url)
+            return send_file('./images.zip', as_attachment=True)
+        elif scraping_option == 'link-scraping':
+            scraped_data = scrape_hyperlinks(url)
+            return render_template('result.html', data=scraped_data)
+    
+    return render_template('scrape.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
